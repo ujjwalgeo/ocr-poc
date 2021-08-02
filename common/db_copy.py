cefbd03ed121511a254c4e62e4b8ac867c95a8a5
Ujjwal Narayan <ujjwal.narayan@gmail.com>
2021-08-02 15:47:14 -0700

    db copy

--- common/db_copy.py
+++ common/db_copy.py
@@ -8,7 +8,7 @@ source_mongo = MONGO_DB
 target_mongo = 'mongodb://localhost:27017'
 source_db = 'chicago_big1'
 target_db = 'chicago_big1'
-batch_size = 100
+page_size = 100
 
 source_client = MongoClient(source_mongo)
 source_db = source_client[source_db]
@@ -19,7 +19,21 @@ target_db = target_client[target_db]
 collections = [ASBUILTS_COLLECTION, OCR_LINE_COLLECTION, AZURE_ANALYSIS_COLLECTION]
 
 
-def idlimit(collection, page_size, last_id=None):
+def skiplimit(collection, page_num):
+    """returns a set of documents belonging to page number `page_num`
+    where size of each page is `page_size`.
+    """
+    # Calculate number of documents to skip
+    skips = page_size * (page_num - 1)
+
+    # Skip and limit
+    cursor = collection.find().skip(skips).limit(page_size)
+
+    # Return documents
+    return [x for x in cursor]
+
+
+def idlimit(collection, last_id=None):
     """Function returns `page_size` number of documents after last_id
     and the new last_id.
     """
@@ -50,9 +64,22 @@ def copy_collection(collection):
 
     data, last_id = idlimit(source_docs)
     while not (last_id is None):
-        target_docs.insert_many(data, ordering=False)
+        target_docs.insert_many(data, ordered=False)
         data, last_id = idlimit(source_docs, last_id)
         log.info(last_id)
+
+
+def copy_collection1(collection):
+    source_docs = source_db[collection]
+    target_docs = target_db[collection]
+    num_source_docs = source_docs.count()
+    num_pages = 1 + int(num_source_docs / page_size)
+
+    page_num = 1
+    while page_num <= num_pages:
+        data = skiplimit(source_docs, page_num)
+        target_docs.insert_many(data)
+        page_num += 1
 
 
 if __name__ == "__main__":
